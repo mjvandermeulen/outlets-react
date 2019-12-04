a1425feacbbefc33db969f07af30f30615257135
Maarten Jan van der Meulen <mjvandermeulen@gmail.com>
2019-12-03 20:31:06 -0600

    added socket.io packages added skelotonish client function. Stashy commit

--- src/components/OutletGroups.tsx
+++ src/components/OutletGroups.tsx
@@ -1,13 +1,30 @@
 import * as React from 'react'
+
+import io from 'socket.io-client'
+
 import { Group } from './Group'
 import { groupsSettings, Mode } from '../settings/group-settings'
 
+const socket = io('http://localhost:3000') // TODO stop hardcoding.
+interface SocketData {
+  // TODO: MOVE &
+  // 'outlet_switch'; 'outlet_timer' ; 'outlet_sync_request';
+  group: string
+  mode: boolean
+}
 interface OutletGroupsProps {}
 
 class OutletGroups extends React.Component<OutletGroupsProps, {}> {
   constructor(props: OutletGroupsProps) {
     super(props)
     this.handleOnOffClick = this.handleOnOffClick.bind(this)
+  }
+
+  public componentDidMount() {
+    socket.on('light', (socketData: SocketData) => {
+      // const newState = ...this.state
+      this.setState({})
+    })
   }
 
   private handleOnOffClick(group: string, mode: Mode) {
