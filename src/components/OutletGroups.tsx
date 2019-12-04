624f24431c1efe092ed9a43331a651a61a6a4cf9
Maarten Jan van der Meulen <mjvandermeulen@gmail.com>
2019-12-04 11:19:18 -0600

    Basic socket.io test working mac to RPi, using create-react-app proxy

--- src/components/OutletGroups.tsx
+++ src/components/OutletGroups.tsx
@@ -5,29 +5,51 @@ import io from 'socket.io-client'
 import { Group } from './Group'
 import { groupsSettings, Mode } from '../settings/group-settings'
 
-const socket = io('http://localhost:3000') // TODO stop hardcoding.
+// TODO This is the real interface
+// interface SocketData {
+//   // TODO: MOVE &
+//   // 'outlet_switch'; 'outlet_timer' ; 'outlet_sync_request';
+//   group: string
+//   mode: boolean
+// }
+// TODO remove test interface:
 interface SocketData {
-  // TODO: MOVE &
-  // 'outlet_switch'; 'outlet_timer' ; 'outlet_sync_request';
   group: string
+  sync: boolean
   mode: boolean
+  timer: number
 }
 interface OutletGroupsProps {}
 
 class OutletGroups extends React.Component<OutletGroupsProps, {}> {
+  private socket: SocketIOClient.Socket | null
   constructor(props: OutletGroupsProps) {
     super(props)
     this.handleOnOffClick = this.handleOnOffClick.bind(this)
+    this.socket = null
   }
 
   public componentDidMount() {
-    socket.on('light', (socketData: SocketData) => {
+    // TODO: Move socket/io('htt..... out of component: always have this connection? ****
+    this.socket = io('http://localhost:3000') // TODO stop hardcoding.
+    this.socket.on('light', (socketData: SocketData) => {
+      console.log(`Received: ${socketData}`)
       // const newState = ...this.state
       this.setState({})
     })
   }
 
   private handleOnOffClick(group: string, mode: Mode) {
+    const socketData: SocketData = {
+      group: 'officelight',
+      sync: false,
+      mode: true,
+      timer: -1,
+    }
+    if (this.socket) {
+      this.socket.emit('light', socketData)
+    }
+
     console.log(`Button clicked: ${group} - ${mode}`)
   }
 
