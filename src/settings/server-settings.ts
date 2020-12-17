8c8f087fbeb7065b847f32da2b1b8f852f2ca48d
Maarten van der Meulen <mjvandermeulen@gmail.com>
2020-12-16 19:43:49 -0600

    pm2 settings, packages updated, gitignore update

--- src/settings/server-settings.ts
+++ src/settings/server-settings.ts
@@ -1,1 +1,7 @@
-export const serverURL = 'http://localhost:3000'
+export const serverURL = 'http://192.168.0.200:3000'
+
+// TODO change to this:
+// export const serverURL = 'http://71.203.213.77:3000'
+// **** TODO make serverURL depend on how the site is called:
+//    - local -> server local as well
+//    - outside -> server outside as well
